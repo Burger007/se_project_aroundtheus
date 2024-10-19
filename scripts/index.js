527002a40e8fb53963a5057595710eeb92520355
Max <maximku97@gmail.com>
2024-10-19 23:59:10 +0300

    udpate

--- scripts/index.js
+++ scripts/index.js
@@ -28,4 +28,9 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditButton.classList.add("modal_opened");
+});
